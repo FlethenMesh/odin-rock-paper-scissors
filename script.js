9493729a5a60a0b2d750dbdfc1e0cb491bcc6775
Waleed Awad <waleed.awad1213@gmail.com>
2023-11-30 01:39:18 -0500

    Creates playGameRound function
    Prompts user for input and checks for "rock", "paper", or "scissors"
    Runs test cases to compare player input with the computer selection
    Assigns game result strings corresponding to comparisons
    Creates console log to output game result to verify function works
    
    Removes console log that tested the getComputerChoice function

--- script.js
+++ script.js
@@ -4,4 +4,33 @@ function getComputerChoice() {
     return computerOptions[randomNumber]; 
 }
 
-console.log("Computer choice: " + getComputerChoice());
+function playGameRound(playerChoice) {
+    let gameResult;
+    const computerChoice = getComputerChoice();
+
+    playerChoice = prompt("Type rock, paper, or scissors: ", "rock, paper, or scissors");
+
+    if (playerChoice.toLowerCase() == "rock" || playerChoice.toLowerCase() == "paper" || playerChoice.toLowerCase() == "scissors") {
+        if (playerChoice.toLowerCase() == "rock" && computerChoice == "scissors") {
+            gameResult = "Computer chose scissors.\nYou win! Rock beats scissors.";
+        } else if (playerChoice.toLowerCase() == "paper" && computerChoice == "rock") {
+            gameResult = "Computer chose rock.\nYou win! Paper beats rock.";
+        } else if (playerChoice.toLowerCase() == "scissors" && computerChoice == "paper") {
+            gameResult = "Computer chose paper.\nYou win! Scissors beats paper."; 
+        } else if (playerChoice.toLowerCase() == "rock" && computerChoice == "paper") {
+            gameResult = "Computer chose paper.\nYou lose! Paper beats rock."; 
+        } else if (playerChoice.toLowerCase() == "paper" && computerChoice == "scissors") {
+            gameResult = "Computer chose scissors.\nYou lose! Scissors beats paper.";
+        } else if (playerChoice.toLowerCase() == "scissors" && computerChoice == "rock") {
+            gameResult = "Computer chose rock.\nYou lose! Rock beats scissors."; 
+        } else {
+            gameResult = "Computer chose " + computerChoice + "\nDraw!";
+        }
+    } else {
+        gameResult = "Invalid game input!";
+    }
+
+    return gameResult;
+}
+
+console.log("Game result: " + playGameRound());
