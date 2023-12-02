a08c83ff960bffcbabc6663c1a3403f74f72c586
Waleed Awad <waleed.awad1213@gmail.com>
2023-12-01 22:09:08 -0500

    Changes function to play round of game
    Adds new function to get player choice.
    This function validates that input is within the options of the game.
    Game round function now returns an array containing message for round
    results and a boolean true/false for win/lose.
    Boolean values will be used in the playGame() function to track score.

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
+function getPlayerChoice(choiceMessage = "Type rock, paper, or scissors: ", playerOptions = ["rock", "paper", "scissors"]) {
+    let playerChoice;
+    
+    do {
+        playerChoice = prompt(choiceMessage, playerOptions.join(', '));
+        playerChoice = playerChoice ? playerChoice.toLowerCase() : "";
+    } while (!playerOptions.includes(playerChoice));
+    return playerChoice;
+}
+
 function getComputerChoice() {
     const computerOptions = ["rock", "paper", "scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
     return computerOptions[randomNumber]; 
 }
 
-function playGameRound(playerChoice) {
-    let gameResult;
+function playGameRound() {
+    let gameRoundResult;
+    let gameRoundMessage;
+
+    const playerChoice = getPlayerChoice();
+
     const computerChoice = getComputerChoice();
 
-    playerChoice = prompt("Type rock, paper, or scissors: ", "rock, paper, or scissors");
-
-    if (playerChoice.toLowerCase() == "rock" || playerChoice.toLowerCase() == "paper" || playerChoice.toLowerCase() == "scissors") {
-        if (playerChoice.toLowerCase() == "rock" && computerChoice == "scissors") {
-            gameResult = "Computer chose scissors.\nYou win! Rock beats scissors.";
-        } else if (playerChoice.toLowerCase() == "paper" && computerChoice == "rock") {
-            gameResult = "Computer chose rock.\nYou win! Paper beats rock.";
-        } else if (playerChoice.toLowerCase() == "scissors" && computerChoice == "paper") {
-            gameResult = "Computer chose paper.\nYou win! Scissors beats paper."; 
-        } else if (playerChoice.toLowerCase() == "rock" && computerChoice == "paper") {
-            gameResult = "Computer chose paper.\nYou lose! Paper beats rock."; 
-        } else if (playerChoice.toLowerCase() == "paper" && computerChoice == "scissors") {
-            gameResult = "Computer chose scissors.\nYou lose! Scissors beats paper.";
-        } else if (playerChoice.toLowerCase() == "scissors" && computerChoice == "rock") {
-            gameResult = "Computer chose rock.\nYou lose! Rock beats scissors."; 
-        } else {
-            gameResult = "Computer chose " + computerChoice + "\nDraw!";
-        }
+    if ((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "scissors" && computerChoice == "paper")) {
+        gameRoundMessage = "Computer chose " + computerChoice + "\n=> You win!\n" + playerChoice + " beats " + computerChoice;
+        gameRoundResult = true; //True for win conditions
+    } else if ((playerChoice == "rock" && computerChoice == "paper") || (playerChoice == "paper" && computerChoice == "scissors") || (playerChoice == "scissors" && computerChoice == "rock")) {
+        gameRoundMessage = "Computer chose " + computerChoice +  + "\n=> You lose!\n" + computerChoice + " beats " + playerChoice;
+        gameRoundResult = false; //False for lose conditions
     } else {
-        gameResult = "Invalid game input!";
+        const nextRound = playGameRound();
+        gameRoundResult = nextRound.result;
+        gameRoundMessage = "Computer chose " + computerChoice + "\n=> Draw!\n" + nextRound.message;
     }
 
-    return gameResult;
+    return { result: gameRoundResult, message: gameRoundMessage };
 }
 
-console.log("Game result: " + playGameRound());
+const result = playGameRound();
+console.log("Game result: " + result.result);
+console.log(result.message);
